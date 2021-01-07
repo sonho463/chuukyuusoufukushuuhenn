39f89943c73b6cd485acfddd31b1d356ad2c7ec1
牧大介 <raki0401@outlook.jp>
2021-01-07 22:11:08 +0900

    jQuery動画　2:28秒から

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
+
+//drawer
 $(function(){
   //drawerjs
   $('.drawer').drawer();
 })
+
+
+//スムーススクロール
+$(function(){
+  $('a[href^="#"]').click(function(){
+    let header = $(".header").innerHeight();
+    let speed = 300;
+    let id = $(this).attr("href");
+    let target = $("#" == id ? "html" : id);
+    let position = $(target).offset().top - header;
+    $("html,body").animate({scrollTop : position  },speed);
+    return false;
+  });
+});
+
+//wow-js
+$(function(){
+  new WOW().init();
+});
