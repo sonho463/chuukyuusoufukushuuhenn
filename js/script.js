47c285e4507fe2daf8de4c70ba0043b7354594b7
sonho463 <son.ho463@gmail.com>
2021-01-09 16:05:55 +0900

    -> form

--- js/script.js
+++ js/script.js
@@ -28,29 +28,29 @@ $(function(){
 $(function(){
 
   let form = $('#js-form')
-  $form.submit(function(e) { 
-    $.ajax({ 
-     url: $form.attr('action'), 
-     data: $form.serialize(), 
-     type: "POST", 
-     dataType: "xml", 
-     statusCode: { 
-        0: function() { 
-          //送信に成功したときの処理 
-          $form.slideUp()
+  form.submit(function(e) {
+    $.ajax({
+     url: form.attr('action'),
+     data: form.serialize(),
+     type: "POST",
+     dataType: "xml",
+     statusCode: {
+        0: function() {
+          //送信に成功したときの処理
+          $('#form').slideUp()
           $('#js-success').slideDown()
-        }, 
-        200: function() { 
-          //送信に失敗したときの処理 
-          $form.slideUp()
+        },
+        200: function() {
+          //送信に失敗したときの処理
+          $('#form').slideUp()
           $('#js-error').slideDown()
-        } 
-      } 
+        }
+      }
     });
-    
+
     e.preventDefault();
-    return false; 
-  }); 
+    return false;
+  });
 });
 
 
