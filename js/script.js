fcc4c1afb771f69560147c33a1ac5b9e4cea52f1
牧大介 <raki0401@outlook.jp>
2021-01-09 10:38:32 +0900

    google formが画面遷移するので止まっている

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ $(function(){
 $(function(){
   $('a[href^="#"]').click(function(){
     let header = $(".header").innerHeight();
-    let speed = 300;
+    let speed = 700;
     let id = $(this).attr("href");
-    let target = $("#" == id ? "html" : id);
+    let target = $("#" == id ? "html" : id);     // 三項演算子
     let position = $(target).offset().top - header;
     $("html,body").animate({scrollTop : position  },speed);
     return false;
@@ -23,3 +23,49 @@ $(function(){
 $(function(){
   new WOW().init();
 });
+
+//google form
+$(function(){
+
+  let form = $('#js-form')
+  $form.submit(function(e) { 
+    $.ajax({ 
+     url: $form.attr('action'), 
+     data: $form.serialize(), 
+     type: "POST", 
+     dataType: "xml", 
+     statusCode: { 
+        0: function() { 
+          //送信に成功したときの処理 
+          $form.slideUp()
+          $('#js-success').slideDown()
+        }, 
+        200: function() { 
+          //送信に失敗したときの処理 
+          $form.slideUp()
+          $('#js-error').slideDown()
+        } 
+      } 
+    });
+    
+    e.preventDefault();
+    return false; 
+  }); 
+});
+
+
+
+// formの入力
+// let submit = $('#js-submit')
+// $('#js-form input, #js-form textarea').on('change', function(){
+//   if(
+//     $('#js-form input[type="text"]').val() !== "" &&
+//     $('#js-form input[type="email"]').val() !== "" &&
+//     $('#js-form input[name="entry.577350080"]').prop('checked') === true
+//   ){
+//     $submit.addClass('-active')
+//   } else{
+//     $submit.removeClass('-active')
+
+//   }
+// });
